1e211defa560c3b4d1fce591f404e95538f595d1
fade2g <holger.heymanns@gmail.com>
2017-06-15 10:35:44 +0200

    Use sample with less color disturbance

--- src/main.cpp
+++ src/main.cpp
@@ -1,16 +1,17 @@
-//
-// Created by Holger on 15.06.2017.
-//
+// Test for using attiny85 with platformio
+// code is copy & paste from https://github.com/FastLED/FastLED/blob/master/examples/Cylon/Cylon.ino
+// Hardware is a ring of 12 ws2812 RGB LED, order of colors is GRB
 
-#include <Arduino.h>
-#include <FastLED.h>
+// used clion IDE for development. Build throws error (incl. upload) but it still works somehow. Feels like npm install :-)
+
+#include "FastLED.h"
 
 // How many leds in your strip?
 #define NUM_LEDS 12
 
 // For led chips like Neopixels, which have a data line, ground, and power, you just
 // need to define DATA_PIN.  For led chipsets that are SPI based (four wires - data, clock,
-// ground, and power), like the LPD8806 define both DATA_PIN and CLOCK_PIN
+// ground, and power), like the LPD8806, define both DATA_PIN and CLOCK_PIN
 #define DATA_PIN 0
 #define CLOCK_PIN 13
 
@@ -18,46 +19,41 @@
 CRGB leds[NUM_LEDS];
 
 void setup() {
-    // Uncomment/edit one of the following lines for your leds arrangement.
-    // FastLED.addLeds<TM1803, DATA_PIN, RGB>(leds, NUM_LEDS);
-    // FastLED.addLeds<TM1804, DATA_PIN, RGB>(leds, NUM_LEDS);
-    // FastLED.addLeds<TM1809, DATA_PIN, RGB>(leds, NUM_LEDS);
-    // FastLED.addLeds<WS2811, DATA_PIN, RGB>(leds, NUM_LEDS);
-    FastLED.addLeds<WS2812, DATA_PIN, GRB>(leds, NUM_LEDS);     // Changed this for attiny85, was RBB but hey, colors where mixed up
-    // FastLED.addLeds<WS2812B, DATA_PIN, RGB>(leds, NUM_LEDS);
-    // FastLED.addLeds<NEOPIXEL, DATA_PIN>(leds, NUM_LEDS);
-    // FastLED.addLeds<APA104, DATA_PIN, RGB>(leds, NUM_LEDS);
-    // FastLED.addLeds<UCS1903, DATA_PIN, RGB>(leds, NUM_LEDS);
-    // FastLED.addLeds<UCS1903B, DATA_PIN, RGB>(leds, NUM_LEDS);
-    // FastLED.addLeds<GW6205, DATA_PIN, RGB>(leds, NUM_LEDS);
-    // FastLED.addLeds<GW6205_400, DATA_PIN, RGB>(leds, NUM_LEDS);
-
-    // FastLED.addLeds<WS2801, RGB>(leds, NUM_LEDS);
-    // FastLED.addLeds<SM16716, RGB>(leds, NUM_LEDS);
-    // FastLED.addLeds<LPD8806, RGB>(leds, NUM_LEDS);
-    // FastLED.addLeds<P9813, RGB>(leds, NUM_LEDS);
-    // FastLED.addLeds<APA102, RGB>(leds, NUM_LEDS);
-    // FastLED.addLeds<DOTSTAR, RGB>(leds, NUM_LEDS);
-
-    // FastLED.addLeds<WS2801, DATA_PIN, CLOCK_PIN, RGB>(leds, NUM_LEDS);
-    // FastLED.addLeds<SM16716, DATA_PIN, CLOCK_PIN, RGB>(leds, NUM_LEDS);
-    // FastLED.addLeds<LPD8806, DATA_PIN, CLOCK_PIN, RGB>(leds, NUM_LEDS);
-    // FastLED.addLeds<P9813, DATA_PIN, CLOCK_PIN, RGB>(leds, NUM_LEDS);
-    // FastLED.addLeds<APA102, DATA_PIN, CLOCK_PIN, RGB>(leds, NUM_LEDS);
-    // FastLED.addLeds<DOTSTAR, DATA_PIN, CLOCK_PIN, RGB>(leds, NUM_LEDS);
+    Serial.begin(57600);
+    Serial.println("resetting");
+    LEDS.addLeds<WS2812,DATA_PIN,GRB>(leds,NUM_LEDS);
+    LEDS.setBrightness(84);
 }
 
+void fadeall() { for(int i = 0; i < NUM_LEDS; i++) { leds[i].nscale8(250); } }
+
 void loop() {
-    leds[0] = CRGB::Red;
-    FastLED.show();
-    delay(200);
-    leds[0] = CRGB::Green;
-    FastLED.show();
-    delay(200);
-    leds[0] = CRGB::Blue;
-    FastLED.show();
-    delay(200);
-    leds[0] = CRGB::Black;
-    FastLED.show();
-    delay(2000);
+    static uint8_t hue = 0;
+    Serial.print("x");
+    // First slide the led in one direction
+    for(int i = 0; i < NUM_LEDS; i++) {
+        // Set the i'th led to red
+        leds[i] = CHSV(hue++, 255, 255);
+        // Show the leds
+        FastLED.show();
+        // now that we've shown the leds, reset the i'th led to black
+        // leds[i] = CRGB::Black;
+        fadeall();
+        // Wait a little bit before we loop around and do it again
+        delay(10);
+    }
+    Serial.print("x");
+
+    // Now go in the other direction.
+    for(int i = (NUM_LEDS)-1; i >= 0; i--) {
+        // Set the i'th led to red
+        leds[i] = CHSV(hue++, 255, 255);
+        // Show the leds
+        FastLED.show();
+        // now that we've shown the leds, reset the i'th led to black
+        // leds[i] = CRGB::Black;
+        fadeall();
+        // Wait a little bit before we loop around and do it again
+        delay(10);
+    }
 }
